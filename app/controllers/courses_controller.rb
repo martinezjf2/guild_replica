341d4a92370e33bdfd1a6ad5b3d288b5aa00ae4b
Jeffrey Martinez <martinezjf2@gmail.com>
2020-12-08 14:35:30 -0500

    added actions to my courses controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,36 @@
 class CoursesController < ApplicationController
+    def index
+        @course = Courses.all
+    end
+
+    def new
+        @course = Course.new
+    end
+
+    def create
+
+    end
+
+    def show
+        @course = Course.find_by(id: params[:id])
+    end
+
+    def edit
+        @course = Course.find_by(id: params[:id])
+    end
+
+    def update
+
+    end
+
+    def destroy
+        
+    end
+
+    private
+
+    def course_params
+        # params.require(:course).permit(#list attribute in keys)
+    end
+
 end
