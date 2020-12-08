a274a5ae197de7991fb1a63fd0ef62abe1d44110
Jeffrey Martinez <martinezjf2@gmail.com>
2020-12-08 14:31:54 -0500

    vadded validations to the course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,10 @@
 class Course < ApplicationRecord
+    belongs_to :student
+    validates :description, presence: {message: "can't be empty"}
+    validates :grade, presence: {message: "can't be empty"}
+    validates :year_taken, :presence: {message: "can't be empyty"}
+    
+    def make_grade_to_i
+# did not complete method due to the counselor can put college grades such as A, B, C
+    end
 end
