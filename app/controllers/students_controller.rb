0cd705f1f9479237176322ab6dd2d83b370dce61
Jeffrey Martinez <martinezjf2@gmail.com>
2020-12-08 14:36:29 -0500

    add actions to students controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,31 @@
 class StudentsController < ApplicationController
+    def home
+
+    end
+
+    def index
+
+    end
+
+    def new
+        @student = Student.new
+    end
+
+    def create
+
+    end
+
+    def edit
+    
+    end
+
+    def update
+
+    end
+
+    private
+
+    def student_params
+        # params.require(:user).permit(:first_name, :last_name, :email, :username, :password, :password_confirmation)
+    end
 end
