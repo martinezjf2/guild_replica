a717fabde822dd041c57536e82f1ae3c0753cfb8
Jeffrey Martinez <martinezjf2@gmail.com>
2020-12-11 16:58:25 -0500

    added on the sessions controller for logging in and logging out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,19 @@ class SessionsController < ApplicationController
     end
 
     def create
-        
-
+        @student = Student.find_by(username: params[:student][:username])
+        if @student && @student.authenticate(params[:student][:password])
+            session[:student_id] = @student.id
+            redirect_to students_path
+        else
+            flash[:message] = "Sorry, Please Try Again"
+            render :new
+        end
     end
 
     def destroy
+        session.delete :student_id
+        redirect_to "/"
 
     end
 
