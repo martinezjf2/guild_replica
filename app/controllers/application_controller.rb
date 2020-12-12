9b6813ed12eea63a736fb8bb1acc8af950689838
Jeffrey Martinez <martinezjf2@gmail.com>
2020-12-12 16:25:52 -0500

    added helper methods for the current student and require login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_student
+
+    def current_student
+        Student.find_by(id: params[:student_id])
+    end
+
+    def require_login
+        if current_student
+            current_student
+        else
+            redirect_to '/'
+        end
+    end
+    
 end
