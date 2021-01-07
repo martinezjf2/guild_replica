8303bb173ab1ab0531e5b9aea1a2ce3b553caffc
Jeffrey Martinez <martinezjf2@gmail.com>
2021-01-06 23:22:04 -0500

    testing flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,11 @@ class SessionsController < ApplicationController
         @student = Student.find_by(username: params[:student][:username])
         if @student && @student.authenticate(params[:student][:password])
             session[:student_id] = @student.id
-            redirect_to student_path(@student)
+            redirect_to student_path(@student), notice: "You are successfully logged in!"
             # When logging in i encounter that in order to login with enter, i have to refresh the page and then input the values
         else
-            flash[:message] = "Sorry, Please Try Again"
-            redirect_to '/login'
+            # flash[:alert] = "Sorry, Please Try Again"
+            redirect_to '/login', alert: "User not found. Please Try Again"
         end
     end
 
