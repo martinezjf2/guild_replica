e05fad937c318f7c2ac40737139018f9df7c643e
Jeffrey Martinez <martinezjf2@gmail.com>
2021-01-07 12:46:56 -0500

    added the student errors to the students view directory

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,25 @@
 class SessionsController < ApplicationController
+    before_action :redirect_to_login, only: [:new]
 
     def new
-
+        
     end
 
     def create
         @student = Student.find_by(username: params[:student][:username])
         if @student && @student.authenticate(params[:student][:password])
             session[:student_id] = @student.id
-            redirect_to student_path(@student), notice: "You are successfully logged in!"
-            # When logging in i encounter that in order to login with enter, i have to refresh the page and then input the values
+            redirect_to student_path(@student), notice: "You are Successfully Logged In!"
+
         else
-            # flash[:alert] = "Sorry, Please Try Again"
             redirect_to '/login', alert: "User not found. Please Try Again"
+
         end
     end
 
     def destroy
         session.delete :student_id
-        redirect_to "/", notice: "You 've been logged out successfully!"
+        redirect_to "/", notice: "You've been logged out Successfully!"
 
     end
 
