d68f4ad19c0859f52dd0dcec2e7d135e65324ff5
Jeffrey Martinez <martinezjf2@gmail.com>
2021-01-08 20:22:14 -0500

    added danger or success flash types to the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,17 +9,17 @@ class SessionsController < ApplicationController
         @student = Student.find_by(username: params[:student][:username])
         if @student && @student.authenticate(params[:student][:password])
             session[:student_id] = @student.id
-            redirect_to student_path(@student), notice: "You are Successfully Logged In!"
+            redirect_to student_path(@student), success: "You are Successfully Logged In!"
 
         else
-            redirect_to '/login', alert: "User not found. Please Try Again"
+            redirect_to '/login', danger: "User not found. Please Try Again"
 
         end
     end
 
     def destroy
         session.delete :student_id
-        redirect_to "/", notice: "You've been logged out Successfully!"
+        redirect_to "/", success: "You've been logged out Successfully!"
 
     end
 
