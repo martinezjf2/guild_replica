bd84700344a706fc8aa44b9a2b248a85953a365e
Jeffrey Martinez <martinezjf2@gmail.com>
2021-02-26 12:10:51 -0500

    added the redirect to student path on the student#home

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,7 @@ class StudentsController < ApplicationController
     before_action :require_login, only: [:show, :edit, :update]
 
     def home
-
+        redirect_to student_path(current_student.id)
     end
 
     def show
