ed2fe6e3e4a91c54b8798b905addc9df44f274fa
Jeffrey Martinez <martinezjf2@gmail.com>
2021-02-27 23:28:04 -0500

    want to add unregistered course back to the registering page

--- app/controllers/students_registered_courses_controller.rb
+++ app/controllers/students_registered_courses_controller.rb
@@ -7,8 +7,21 @@ class StudentsRegisteredCoursesController < ApplicationController
     end
 
     def destroy
+        @register_courses = RegisterCourse.all
         @course = current_student.register_courses.find_by(id: params[:id])
         @course.destroy
+        # binding.pry
+        # @register_course.push(@course)
+        # @register_courses << RegisterCourse.new(name: @course.name, end: @course.end, start: @course.start, description: @course.description)
+        # binding.pry
         redirect_to student_courses_path(current_student.id)
     end
+
+    private
+
+    # def register_course_params
+        
+    #     params.require(:register_course).permit(:name, :end, :start, :description)
+          
+    # end
 end
