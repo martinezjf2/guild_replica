2f4793d7e17205b482bf5c9e9446906c153ba1b6
Jeffrey Martinez <martinezjf2@gmail.com>
2021-02-28 20:48:37 -0500

    centerd the image upload with padding

--- app/controllers/students_registered_courses_controller.rb
+++ app/controllers/students_registered_courses_controller.rb
@@ -4,32 +4,14 @@ class StudentsRegisteredCoursesController < ApplicationController
     def show
         # @course = RegisterCourse.find_by(id: params[:id])
         @course = current_student.register_courses.find_by(id: params[:id])
-        binding.pry
         # invoke something here to pass the register_id as a param to the button, 
     end
 
     def destroy
-        binding.pry
         @course = current_student.register_courses.find_by(id: params[:id])
         @course.delete
-        # @register_course.push(@course)
-        # @register_courses << RegisterCourse.new(name: @course.name, end: @course.end, start: @course.start, description: @course.description)
         # binding.pry
         redirect_to student_courses_path(current_student.id)
     end
 
-    # def destroy
-    #     # @course = StudentRegisteredCourse.find_by(id: params[:id])
-    #     @course = current_student.students_registered_courses.find_by(id: params[:id])
-    #     @course.destroy
-    #     # @register_course.push(@course)
-    #     # @register_courses << RegisterCourse.new(name: @course.name, end: @course.end, start: @course.start, description: @course.description)
-    #     redirect_to student_courses_path(current_student.id)
-    # end
-
-    private
-
-#     def register_course_params     
-#         params.permit(:register_course_id)     
-#     end
 end
